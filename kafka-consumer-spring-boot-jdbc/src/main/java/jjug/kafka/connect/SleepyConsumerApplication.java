734e6f9aedc9922a1e02faf62114a738331a5c3b
Barry Evans <barryevans80@gmail.com>
2025-05-12 19:59:23 +0900

    Step 3.2 - Add Spring Boot Kafka Consumer/JDBC Project
    The project is built with gradle and uses Java 21, Spring Kafka, and
    Spring JDBC, and kafka-avro-serializer library to consume Avro records.
    
    A simple @KafkaListener is used to consume records, and JdbcOperations
    is injected to save the data to the database.
    
    Spring Boot application.yaml has been added with the kafka broker
    and JDBC data source configuration.

---
+++ kafka-consumer-spring-boot-jdbc/src/main/java/jjug/kafka/connect/SleepyConsumerApplication.java
@@ -0,0 +1,13 @@
+package jjug.kafka.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepyConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepyConsumerApplication.class, args);
+    }
+
+}
