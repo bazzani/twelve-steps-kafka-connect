74e210454b93b1b3109da59ca800ba0457342a63
Barry Evans <barryevans80@gmail.com>
2025-05-20 00:13:04 +0900

    Step 3.1 - Add kafka-consumer-sprint-0
    - use slf4j-simple to enable logging

---
+++ kafka-consumer-sprint-0/src/main/java/jjug/kafka/connect/KafkaConsumerSprint0.java
@@ -0,0 +1,44 @@
+package jjug.kafka.connect;
+
+import org.apache.avro.generic.GenericData;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaConsumerSprint0 {
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumerSprint0.class);
+
+    public static void main(String[] args) {
+        var config = getConfig();
+
+        try (KafkaConsumer<String, GenericData.Record> consumer = new KafkaConsumer<>(config)) {
+            consumer.subscribe(Collections.singletonList("pizza_orders"));
+
+            while (true) {
+                ConsumerRecords<String, GenericData.Record> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, GenericData.Record> kafkaRecord : records) {
+                    log.info("Consumed Kafka record: offset [{}], key [{}], value [{}]]", kafkaRecord.offset(), kafkaRecord.key(), kafkaRecord.value());
+                }
+            }
+        }
+    }
+
+    private static Properties getConfig() {
+        var config = new Properties();
+
+        config.put("bootstrap.servers", "localhost:9092");
+        config.put("auto.offset.reset", "latest");
+        config.put("group.id", "jjug-kafka-connect-sprint-0-0");
+        config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        config.put("value.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        config.put("schema.registry.url", "http://localhost:8081");
+
+        return config;
+    }
+}
